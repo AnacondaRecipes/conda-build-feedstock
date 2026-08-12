ce96a99fce8e1b5c16b96ef19a8d8a972ab23842
Eric Lundby <Eric.Lundby@gmail.com>
2026-08-12 12:52:06 -0600

    Use conda-launchers. instead of compiling.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,30 @@
-:: Build ARM64 launcher executable
-pushd conda_build\launcher_sources
-curl -SLo launcher.c https://raw.githubusercontent.com/python/cpython/3.7/PC/launcher.c
-patch -p0 < cpython-launcher-c-mods-for-setuptools.3.7.patch
-
-echo #include "winuser.h" > resources.rc
-echo 1 RT_MANIFEST manifest.xml >> resources.rc
-rc /nologo resources.rc
-
-cl.exe /nologo /O1 /Os /MT /DNDEBUG /DSCRIPT_WRAPPER /DUNICODE /D_UNICODE /DWIN32_LEAN_AND_MEAN /GL /GS- /Gy launcher.c resources.res /link /MACHINE:ARM64 /SUBSYSTEM:CONSOLE /OPT:REF /OPT:ICF /LTCG advapi32.lib shell32.lib version.lib /OUT:cli-arm64.exe
-copy cli-arm64.exe ..\
-popd
-
 "%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit /b 1
+
+REM Copy launchers from the conda-launchers host dependency into the package.
+set "ARCH_SUFFIX=64"
+if "%SUBDIR%"=="win-arm64" set "ARCH_SUFFIX=arm64"
+if "%SUBDIR%"=="win-32" set "ARCH_SUFFIX=32"
+
+set "LAUNCHER_SRC=%PREFIX%\share\conda-launchers"
+set "LAUNCHER_DST=%SP_DIR%\conda_build"
+
+if not exist "%LAUNCHER_SRC%\cli-%ARCH_SUFFIX%.exe" (
+    echo ERROR: missing %LAUNCHER_SRC%\cli-%ARCH_SUFFIX%.exe from conda-launchers
+    exit /b 1
+)
+
+copy /Y "%LAUNCHER_SRC%\cli-%ARCH_SUFFIX%.exe" "%LAUNCHER_DST%\"
+if errorlevel 1 exit /b 1
+copy /Y "%LAUNCHER_SRC%\gui-%ARCH_SUFFIX%.exe" "%LAUNCHER_DST%\"
+if errorlevel 1 exit /b 1
+
+REM win-64 builds also ship the native ARM64 launcher for cross-targeting.
+if "%SUBDIR%"=="win-64" (
+    if exist "%LAUNCHER_SRC%\cli-arm64.exe" (
+        copy /Y "%LAUNCHER_SRC%\cli-arm64.exe" "%LAUNCHER_DST%\"
+        if errorlevel 1 exit /b 1
+        copy /Y "%LAUNCHER_SRC%\gui-arm64.exe" "%LAUNCHER_DST%\"
+        if errorlevel 1 exit /b 1
+    )
+)
